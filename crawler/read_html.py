49831af277edc739f8c99761d7778c4abf80b674
RenAlex <ar7671@pleasantonusd.net>
2019-06-29 21:04:17 -0700

    check in

--- crawler/read_html.py
+++ crawler/read_html.py
@@ -5,8 +5,7 @@ url = "https://www.swimmingrank.com/zone/usa/scy_girls_5_8_50FR.html"
 res = requests.get(url)
 soup = BeautifulSoup(res.content, 'html.parser')
 table = soup.find_all('table')[0]
-df = pd.read_html(str(table))[0]
-print(df)
+df = pd.read_html(str(table), header=0, index_col=0)[0]
 
 from sqlalchemy import create_engine
 import sqlite3
@@ -14,3 +13,4 @@ engine = create_engine('sqlite:////Users/alexren/projects/projectX/db')
 df.index.names = ['id']
 df = df.rename({'Swim Meet': 'Swim_Meet'}, axis=1)
 df.to_sql('US_Rankings', con=engine, if_exists='replace')
+print(df)
