4c28353d224f1f6d6ac05df5dd0a7dbc45c09358
John Huang <huang@huang-a03.vmware.com>
2019-07-26 21:36:50 -0700

    add DB_URI

--- server/config.py
+++ server/config.py
@@ -6,9 +6,9 @@ class Config:
     AppName="Swim Cloud"
     DEFAULT_PAGE_LIMIT = 5
     # Alex's local DB
-    # DB_URI = "sqlite:////Users/alexren/projects/projectX/db"
+    DB_URI = "sqlite:////Users/alexren/projects/projectX/db"
     # John's local DB
-    DB_URI = "sqlite:////Users/huang/Documents/_Workspace/projectx/db"
+    # DB_URI = "sqlite:////Users/huang/Documents/_Workspace/projectx/db"
     # Production DB
     # DB_URI = "postgres://bmccpkxvfniseh:d2103f913c6848c73a5d01282e9ee392d2d3320f74ce0eec2deb0d4463916fad@ec2-174-129-227-80.compute-1.amazonaws.com:5432/d2kv5ln1qmovdo"
 
