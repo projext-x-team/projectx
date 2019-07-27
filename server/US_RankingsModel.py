3a6af5bdb14ac7e7d20b48887b7712b0589706fa
John Huang <huang@huang-a03.vmware.com>
2019-07-27 12:57:30 -0700

    add more fields to US_RankingsModel.py

--- server/US_RankingsModel.py
+++ server/US_RankingsModel.py
@@ -6,14 +6,20 @@ db = SQLAlchemy(app)
 
 class US_Rankings(db.Model):
     __tablename__ = 'US_Rankings'
-    id = db.Column(db.BigInteger, primary_key = True)
-    swimmer = db.Column(db.Text)
+    id = db.Column(db.Integer, primary_key = True)
+    swimmer = db.Column(db.String)
     age = db.Column(db.BigInteger)
-    lsc = db.Column(db.Text)
-    club = db.Column(db.Text)
-    time = db.Column(db.Float)
-    date = db.Column(db.Text)
-    swim_meet = db.Column(db.Text)
+    gender = db.Column(db.String)
+    zone = db.Column(db.String)
+    ageGp = db.Column(db.String)
+    lsc = db.Column(db.String)
+    club = db.Column(db.String)
+    event = db.Column(db.String)
+    ranking = db.Column(db.String)
+    course = db.Column(db.String)
+    time = db.Column(db.DateTime)
+    date = db.Column(db.DateTime)
+    swim_meet = db.Column(db.String)
 
     @staticmethod
     def topSwimmers(num):
